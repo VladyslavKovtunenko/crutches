c4a2ba8c3381dd3a3f104007cbe2cc1ea53494fd
someone <er.coooool@gmail.com>
2016-05-24 20:42:39 +0300

    server update

--- backend/app.js
+++ backend/app.js
@@ -5,17 +5,20 @@ var http = require('http');
 var path = require('path');
 var config = require('lib/config');
 var fs = require('fs');
-var dao = require('db/dao');
+// var dao = require('db/dao');
 
 var app = express();
 
-app.set('views', path.join(__dirname, '../frontend/view'));
+http.createServer(app).listen(config.get('server_port'), function () {
+    console.log('server start on ' + config.get('server_port') + ' port');
+});
+
+app.set('views', path.join(__dirname, '../frontend/static'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, '../frontend/static')));
 app.use(bodyParser.json());
 
-app.use(function (req, res) {
-    switch (req.url) {
-    //TODO...
-    }
+app.get('/', function (req, res) {
+    // console.log(res.body);
+    fs.createReadStream('index.html').pipe(res);
 });
--- backend/db/dao.js
+++
@@ -1,5 +0,0 @@
-var sequelize = require('sequelize');
-var connection = require('./connection')(sequelize);
-var creator = require('./creator')(connection, sequelize);
-
-//TODO... exports logic
